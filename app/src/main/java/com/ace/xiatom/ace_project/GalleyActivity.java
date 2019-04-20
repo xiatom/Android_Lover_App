7953ec64eddc6b5f5081abe39d6f3d5ccbf8cec3
xiatom <xiatom@126.com>
2019-04-20 10:36:46 +0800

    更改颜色

---
+++ app/src/main/java/com/ace/xiatom/ace_project/GalleyActivity.java
@@ -0,0 +1,14 @@
+package com.ace.xiatom.ace_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalleyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galley);
+
+    }
+}

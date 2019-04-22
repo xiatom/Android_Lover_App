a5f7c9ae1b9cbd4485b51aa29bad86db3cdf7857
xiatom <xiatom@126.com>
2019-04-22 21:27:36 +0800

    地图定位以及回溯位置

--- app/src/main/java/com/ace/xiatom/ace_project/GPSActivity.java
+++ app/src/main/java/com/ace/xiatom/ace_project/GPSActivity.java
@@ -6,39 +6,120 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
-public class GPSActivity  extends Activity {
-        private MapView mMapView = null;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            SDKInitializer.initialize(getApplicationContext());
+public class GPSActivity extends Activity {
+    BaiduMap mBaiduMap;
+    LocationClient mLocationClient;
+    private MapView mMapView = null;
+    ImageButton locatSelf;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED)
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_PHONE_STATE},1);
-            setContentView(R.layout.activity_gps);
-            //获取地图控件引用
-            mMapView =  findViewById(R.id.bmapView);
-        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_gps);
+        //获取地图控件引用
+        mMapView = findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+
+       // 设置地图属性
+        MyLocationConfiguration mMapConfig = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL
+                ,false, BitmapDescriptorFactory.fromResource(R.drawable.map_marker),
+                0xAAFFFF88,0xAA00FF00);
+        mBaiduMap.setMyLocationConfiguration(mMapConfig);
+
+        mBaiduMap.setMyLocationEnabled(true);
+        mLocationClient = new LocationClient(this);
+
+    //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(2000);
+        mLocationClient.setLocOption(option);
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+        mLocationClient.start();
+        locatSelf = findViewById(R.id.findSelf);
+        locatSelf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng latLng = new LatLng(mLocationClient.getLastKnownLocation().getLatitude(), mLocationClient.getLastKnownLocation().getLongitude());
+                //描述地图状态将要发生的变化,通过当前经纬度来使地图显示到该位置
+                MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+                //改变地图状态
+                mBaiduMap.setMapStatus(msu);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+    }
+
+    class MyLocationListener extends BDAbstractLocationListener {
+
+        Boolean first = true;
+        MyLocationData locData;
         @Override
-        protected void onResume() {
-            super.onResume();
-            //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-            mMapView.onResume();
-        }
-        @Override
-        protected void onPause() {
-            super.onPause();
-            //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-            mMapView.onPause();
-        }
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-            mMapView.onDestroy();
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+
+            mBaiduMap.setMyLocationData(locData);
+
+            if(first){
+                first = false;
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                //描述地图状态将要发生的变化,通过当前经纬度来使地图显示到该位置
+                MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+                //改变地图状态
+                mBaiduMap.setMapStatus(msu);
+            }
         }
     }
+}
